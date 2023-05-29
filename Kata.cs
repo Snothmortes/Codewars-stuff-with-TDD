8c0e4cd02a82500e47acf560d44626f4cf586b36
Glen <72626315+Snothmortes@users.noreply.github.com>
2023-05-29 13:07:02 +0200

    Fixed remainder

--- Kata.cs
+++ Kata.cs
@@ -13,29 +13,35 @@
 
       if (j < 0) {
         str[i] = longestStr[i];
+
         if (remainder == '1') {
-          str[i] += '1';
+          str[i] += (char)1;
           remainder = '0';
         }
       }
       else {
         int sum = longestStr[i] + shortestStr[j] - 0x60;
-        str[i] = (char)(sum + 0x30);
+        str[i] += (char)sum;
 
         if (remainder == '1') {
-          str[i] += '1';
+          str[i] += (char)1;
           remainder = '0';
         }
 
         if (sum >= 10) {
-          str[i] = (char)(sum + 0x26);
+          str[i] -= (char)0xA; // -0xA
           remainder = '1';
         }
 
-        if (i == 0 && remainder == '1') {
-          Array.Resize(ref str, str.Length + 1);
-          Array.Copy(str, 0, str, 1, str.Length - 1);
-          str[i] = '1';
+        str[i] += '0'; // +0x30
+
+        if (i == 0) {
+          if (remainder == '1') {
+            Array.Resize(ref str, str.Length + 1);
+            Array.Copy(str, 0, str, 1, str.Length - 1);
+            str[i] = '\0';
+            str[i] += '1';
+          }
         }
       }
     }
